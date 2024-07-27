19cdb1ce1c3de8ef93fce42af90fa7331bb69072
Charlotte-Greenaway <charlottegreenaway654@gmail.com>
2024-07-27 17:43:31 +0100

    changing icons / adding furry friends

--- src/_components/projects.tsx
+++ src/_components/projects.tsx
@@ -2,18 +2,32 @@ import ProjectComponent from "./projectComponent";
 const Projects = () => {
   const projectList = [
     {
+      title: "Purrfect Toys",
+      logo: "/images/purrfecttoys.png",
+      summary:"E-commerce site built with Sanity, Next auth and Paypal",
+      link:"https://purrfecttoys.charlotte-greenaway.com/",
+      languages:["nextjs","sanity","tailwindcss"]
+    },
+    {
+      title: "Furry Friends",
+      logo: "/images/furry-friends.png",
+      summary:"Pet adoption site built with Astro and tailwind",
+      link:"https://furry-friends.charlotte-greenaway.com/",
+      languages:["astro","tailwindcss"]
+    },
+    {
       title: "Autohub",
       logo: "/images/autohub.png",
       summary:"Dealer Management System, built with Next.js and SQLite",
       link:"https://github.com/Charlotte-Greenaway/Autohub",
-      languages:["next","tailwind", "sqlite"]
+      languages:["nextjs","tailwindcss", "sqlite"]
     },
     {
       title: "Dash Dish",
       logo: "/images/dashdish.png",
       summary:"An AI powered cookbook, built with Next.js and OpenAI's GPT-3",
       link:"https://dashdish.co.uk",
-      languages:["next","openai","mongo","tailwind"]
+      languages:["nextjs","openai","mongodb","tailwindcss"]
     },
     {
       title: "Solar Traveller",
@@ -27,22 +41,16 @@ const Projects = () => {
       logo: "/images/snaptalk.png",
       summary:"Instant messaging service utilising websockets and mongoDB",
       link:"https://github.com/Charlotte-Greenaway/SnapTalk",
-      languages:["react","node", "mongo"]
+      languages:["react","nodejs", "mongodb"]
     },
     {
       title: "First Look",
       logo: "/images/pb.jpg",
       summary:"Wedding photography site built with tailwind and react",
       link:"https://firstlook.charlotte-greenaway.com",
-      languages:["react","tailwind"]
+      languages:["react","tailwindcss"]
     },
-    {
-      title: "Purrfect Toys",
-      logo: "/images/purrfecttoys.png",
-      summary:"E-commerce site built with Sanity, Next auth and Paypal",
-      link:"https://purrfecttoys.charlotte-greenaway.com/",
-      languages:["next","sanity","tailwind"]
-    }
+    
   ];
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
